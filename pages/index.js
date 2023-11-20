dde7cacb9216b51f762ae2411e3a1bc7554f7c35
Satveer Brar <satveerbrar180@gmail.com>
2023-11-20 01:01:00 -0500

    Code changes to make layout response - SHOP NOW button size and text reduced for tablet - Action Heading text size reduced for tablet - Removed Image Collage for mobile screens and fixed some margins

--- pages/index.js
+++ pages/index.js
@@ -14,41 +14,44 @@ const Home = () => {
     <Flex direction="column" bg={globalPageStyles.bg} height="100vh">
       <Navbar {...globalPageStyles} />
       <Divider orientation="horizontal" borderColor="black" w="100%" />
-
       <Flex
         mx={globalPageStyles.mx}
         maxW={globalPageStyles.maxW}
         w={globalPageStyles.w}
         flex="1"
         mt="80px">
-        <Flex direction="column" width="50%">
-          <Heading fontSize="48px" color="black">
+        <Flex direction="column" width={{ base: "80%", md: "50%" }}>
+          <Heading
+            fontSize={{ base: "30px", sm: "30px", md: "36px", lg: "48px" }}
+            color="black">
             Unleashing Confidence Through Advanced Protection
           </Heading>
-          <Heading fontSize="18px" fontWeight="regular" color="black" mt="35px">
+          <Heading
+            fontSize="18px"
+            fontWeight="regular"
+            color="black"
+            mt={{ base: "25px", sm: "25px", md: "30px", lg: "35px" }}>
             Comfort, Confidence, Empowerment.
           </Heading>
           <CommonButton
             text="SHOP NOW"
             bg="#BD7C0A"
-            w="220px"
+            w={{ base: "150px", sm: "150px", md: "180px", lg: "300px" }} // Reduced button size for tablet view
             h="58px"
             fontWeight="bold"
-            fontSize="32px"
+            fontSize={{ base: "20px", sm: "20px", md: "25px", lg: "32px" }} // Reduced font size for tablet view
             boxShadow="4px 4px 4px rgba(0, 0, 0, 1)"
-            mt="35px"
+            mt={{ base: "25px", sm: "25px", md: "30px", lg: "35px" }}
           />
         </Flex>
-        <Flex direction="column" width="50%">
-          <Box>
-            <Image
-              src="/images/collage-image.svg"
-              alt="Collage Image"
-              width="100%"
-              height="auto"
-            />
-          </Box>
-        </Flex>
+        <Box display={{ base: "none", md: "block" }}>
+          <Image
+            src="/images/collage-image.svg"
+            alt="Collage Image"
+            width="100%"
+            height="auto"
+          />
+        </Box>
       </Flex>
     </Flex>
   );
