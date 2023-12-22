080f0c650ad3f69cb357b6483aa6a7c69bfa6f72
Satveer Brar <satveerbrar180@gmail.com>
2023-12-21 22:31:09 -0500

    Cart model updated

--- src/models/cart.js
+++ src/models/cart.js
@@ -1,30 +1,30 @@
 import mongoose from "mongoose"
 const { Schema, models, model } = mongoose
 
+const cartItemSchema = new mongoose.Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "Product",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
 const cartSchema = new mongoose.Schema({
   user: {
     type: Schema.Types.ObjectId,
     required: true,
     ref: "User",
   },
-  items: [
-    {
-      product: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: "Product",
-      },
-      quantity: {
-        type: Number,
-        required: true,
-        min: 1,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-    },
-  ],
+  items: [cartItemSchema],
 })
 
 const Cart = models.Cart || model("Cart", cartSchema)
