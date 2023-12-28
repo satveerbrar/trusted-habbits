d31564dd857e1b90c583fd405b304ae7099738f3
Satveer Brar <satveerbrar180@gmail.com>
2023-12-28 00:59:40 -0500

    Error handling in pages/reviews/index.js - try/catch block added for getStaticProps - return empty list with error message in props if failed to fetch reviews - Box added with Text componenent to display error message instead of Reviews component

--- src/pages/reviews/index.js
+++ src/pages/reviews/index.js
@@ -1,30 +1,46 @@
-import { Flex, Divider } from "@chakra-ui/react"
+import { Flex, Divider, Box, Text } from "@chakra-ui/react"
 import { Navbar } from "@/src/components/Navbar"
 import { Footer } from "@/src/components/Footer"
 import { getReviews } from "@/src/controllers/reviews"
 import { globalPageStyles } from "@/src/utils/staticData"
 import { Reviews } from "@/src/components/Reviews"
 
-const reviews = ({ reviewsData }) => {
+const reviews = ({ reviewsData, error }) => {
   const { bg } = globalPageStyles
 
   return (
     <Flex direction="column" bg={bg} minH="100vh">
       <Navbar {...globalPageStyles} />
       <Divider orientation="horizontal" borderColor="black" w="100%" />
-      <Reviews reviewsData={reviewsData} />
+      {error ? (
+        <Box pt="20" textAlign="center" minH="65vh" bg="white">
+          <Text color="red.500">Sorry failed to load reviews page.</Text>
+        </Box>
+      ) : (
+        <Reviews reviewsData={reviewsData} />
+      )}
       <Footer />
     </Flex>
   )
 }
 
 export async function getStaticProps() {
-  const reviewsData = await getReviews()
+  try {
+    const reviewsData = await getReviews()
 
-  return {
-    props: {
-      reviewsData: reviewsData,
-    },
+    return {
+      props: {
+        reviewsData: reviewsData,
+      },
+    }
+  } catch (error) {
+    console.error("Failed to fetch reviews:", error)
+    return {
+      props: {
+        reviewsData: [],
+        error: "Failed to load reviews.",
+      },
+    }
   }
 }
 
