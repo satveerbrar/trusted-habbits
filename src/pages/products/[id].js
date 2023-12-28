b07b794cf89c7ab7cd2d2fbd1f96d511c6c1e506
Satveer Brar <satveerbrar180@gmail.com>
2023-12-28 01:33:15 -0500

    Error handling added for pages/products/[id].js - try/catch block in getServerSideProps - Product set to null and error message in props when product fetch fails - Used ErrorText component to display message on page

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -5,28 +5,44 @@ import { Navbar } from "@/src/components/Navbar"
 import { Footer } from "@/src/components/Footer"
 import { getProducts } from "@/src/controllers/product"
 import { DetailedProduct } from "@/src/components/Product"
+import { ErrorText } from "@/src/components/ErrorText"
 
-const ProductPage = ({ product }) => {
+const ProductPage = ({ product, error }) => {
   const { bg } = globalPageStyles
 
   return (
     <Flex direction="column" bg={bg} height="auto">
       <Navbar {...globalPageStyles} />
       <Divider orientation="horizontal" borderColor="black" w="100%" />
-      <DetailedProduct product={product} />
+      {error ? (
+        <ErrorText error={error} />
+      ) : (
+        <DetailedProduct product={product} />
+      )}
+
       <Footer />
     </Flex>
   )
 }
 
 export async function getServerSideProps(context) {
-  const { id } = context.params
-  const product = await getProducts(id)
+  try {
+    const { id } = context.params
+    const product = await getProducts(id)
 
-  return {
-    props: {
-      product: product || null,
-    },
+    return {
+      props: {
+        product: product || null,
+      },
+    }
+  } catch (error) {
+    console.error("Failed to fetch product:", error)
+    return {
+      props: {
+        product: null,
+        error: "Sorry server down please check back later",
+      },
+    }
   }
 }
 
